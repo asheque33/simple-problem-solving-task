5660f6ef55f0794359d17cd69bbf5e386c2e0d97
asheque33 <ashequrrahman3333@gmail.com>
2024-06-02 23:30:00 +0600

    for loop example

---
+++ tasks/forLoopExp.js
@@ -0,0 +1,8 @@
+const fruits = ["apple", "banana", "cherry"];
+for (let i = 0; i < fruits.length; i++) {
+  //   console.log(fruits[i]);
+}
+// 2nd example
+for (let i = 0; i < 10; i += 2) {
+  console.log("Hello, World!");
+}
